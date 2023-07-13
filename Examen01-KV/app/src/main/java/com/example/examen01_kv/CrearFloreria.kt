04c9dcd011c8f2d6a0d5fbb6e95e3d48feb82c9e
Karla Vivas <karla.vivas@epn.edu.ec>
2023-07-13 19:08:03 -0400

    Examen pantallas creadas hasta crear floreria

---
+++ Examen01-KV/app/src/main/java/com/example/examen01_kv/CrearFloreria.kt
@@ -0,0 +1,11 @@
+package com.example.examen01_kv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearFloreria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_floreria)
+    }
+}
--- Examen01-KV/app/src/main/java/com/example/examen01_kv/MainActivity.kt
+++ Examen01-KV/app/src/main/java/com/example/examen01_kv/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.examen01_kv
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val botonFlorerias = findViewById<Button>(R.id.btn_florerias)
+        botonFlorerias.setOnClickListener {
+            irActividad(Floreria::class.java)
+        }
+
+        val botonFlores = findViewById<Button>(R.id.btn_flores)
+        botonFlores.setOnClickListener {
+            irActividad(Flor::class.java)
+        }
+    }
+
+    fun irActividad(clase: Class<*>){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
